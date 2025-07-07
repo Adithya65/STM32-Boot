d712b9d280e82de35d6f9a133c89552b7596bfec
Adithya65 <adithyasm6@gmail.com>
2025-07-07 10:28:13 +0530

    FreeRTOS: add freertos tests
    *moved uart init for the console from app to hardware init

--- sources/main.c
+++ sources/main.c
@@ -1,14 +1,15 @@
 #include "stm_blinky.h"
+#include "freertos_tests.h"
 
-#define RUN_FREERTOS_TESTS   0
-#define RUN_STM_SAMPLE       1
+#define RUN_FREERTOS_TESTS   1
+#define RUN_STM_SAMPLE       0
 
 int main()
 {
 #if RUN_STM_SAMPLE
     stm_blinky_app();
 #elif RUN_FREERTOS_TESTS
-    freertos_tests();
+    (void)freertos_tests();
 #else
 #error "Invalid Source"
 #endif
