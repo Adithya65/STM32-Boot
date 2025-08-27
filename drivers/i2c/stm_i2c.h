5bc1db242c3dee3f1bca21b2c088d900032bc133
Adithya SM <63196135+Adithya65@users.noreply.github.com>
2025-08-27 15:30:20 +0530

    Fix styling (#17)

--- drivers/i2c/stm_i2c.h
+++ drivers/i2c/stm_i2c.h
@@ -23,6 +23,6 @@
 
 void i2c_init(void);
 void i2c_stop();
-int32_t i2c_write_data(uint8_t addr, uint8_t* data,uint32_t len, uint8_t start,uint8_t reload);
+int32_t i2c_write_data(uint8_t addr, uint8_t* data, uint32_t len, uint8_t start, uint8_t reload);
 
 #endif
